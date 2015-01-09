c616c0cfa70f0e229e4659acf48d39e8cd75e19d
wcornwell <w.cornwell@unsw.edu.au>
2015-01-09 14:54:13 +1100

    adding color back to the biplot figure

--- fire_biplot.R
+++ fire_biplot.R
@@ -3,6 +3,7 @@
 
 make.fire.biplot<-function(fss=fss){
   ss<-fss$massLoss>50&fss$clade=="g"&!is.na(fss$frontSpeed)
+  colors<-c("red","darkgreen","blue")
   
   fire.out<-cbind(maxTemp=fss$maxTemp[ss],Front.speed=6.25*60/fss$frontSpeed[ss],
                   Burning.time=fss$whole.ring.burning.time[ss],
@@ -13,7 +14,7 @@ make.fire.biplot<-function(fss=fss){
   
   #pdf("Figure4_v1.1.pdf")
   lambda <- PC$sdev * sqrt(nrow(PC$x))
-  plot (t(t(PC$x)/lambda),pch=16,col=1,xlim=c(-0.5,0.5),ylim=c(-0.4,0.4))
+  plot (t(t(PC$x)/lambda),pch=16,col=colors[fss$clade2[ss]],xlim=c(-0.5,0.5),ylim=c(-0.4,0.4))
   par (new=T)
   Rot <- t(t(PC$rotation)*lambda)
   XLIM <- c(-max(abs(Rot[,1])),max(abs(Rot[,1])))
@@ -23,6 +24,6 @@ make.fire.biplot<-function(fss=fss){
   text (Rot[,1:2],rownames(Rot),col=1,cex=0.7)
   axis (3)
   axis (4)
-  #legend("topleft",legend=c("Pinus spp.","cycads","other"),cex=0.7,pch=16,col=1:3)
+  legend("topleft",legend=c("cycads","other","Pinus spp."),cex=0.7,pch=16,col=colors)
   #dev.off()
 }
